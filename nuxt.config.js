221ecfa25fe4718dfb6be9ddedc8034a11e2460c
shivam <shivam.cse17@nituk.ac.in>
2020-09-14 15:08:34 +0530

    change app mode

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,12 +3,7 @@ export default {
    ** Nuxt rendering mode
    ** See https://nuxtjs.org/api/configuration-mode
    */
-  mode: 'spa',
-  /*
-   ** Nuxt target
-   ** See https://nuxtjs.org/api/configuration-target
-   */
-  target: 'static',
+  mode: 'universal',
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
