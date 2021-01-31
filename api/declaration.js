100f4205efe09aa9b8e8c4f3a5e18dbc66b1725a
shivam <shivam.cse17@nituk.ac.in>
2021-01-31 12:42:10 +0530

    Fix broken declarations

--- api/declaration.js
+++ api/declaration.js
@@ -4,7 +4,7 @@ const { writeResponse, parseBody } = require('./utilities')
 async function fetchDeclaration(reqBody, req, res) {
   const version = reqBody.version
   const response = await fetch(
-    `https://github.com/Jozty/Fae/releases/download/${version}/${version}.d.ts`
+    `https://github.com/Jozty/Fae/releases/download/${version}/declarations.d.ts`
   )
   const txtFile = await response.text()
   writeResponse(200, res, txtFile)
