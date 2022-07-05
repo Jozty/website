921d456d1d4ba13cf8c434f275c72feb2b8cbcc0
Shivam Singla <38393640+singla-shivam@users.noreply.github.com>
2022-07-05 13:09:07 +0000

    bump: update latest version

--- plugins/global-data.js
+++ plugins/global-data.js
@@ -1,5 +1,5 @@
 export const globalData = {
   baseUrl: 'https://fae.jozty.io',
-  latestVersion: 'v1.1.0',
-  versions: ['v1.1.0', 'v1.0.0'],
+  latestVersion: 'v1.1.1',
+  versions: ['v1.1.1', 'v1.1.0', 'v1.0.0'],
 }
